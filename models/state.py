9a5bec65d1ff1ac57fd68763fc08f2487fb9cf29
ikede-divine-keno <ikededivine@gmail.com>
2023-08-13 12:03:47 -0700

    models/state.py added

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+    Defines a class State.
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Represent a State class."""
+    name = ""
