675e1eb1f87fa0ea64061a4db0342fc2e43bcf08
ikede-divine-keno <ikededivine@gmail.com>
2023-08-13 11:56:32 -0700

    models/user.py added

---
+++ models/user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+    Defines a class User.
+"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Represent a User
+    Public Attributes:
+    -----------------
+    - email
+    - password
+    - first_name
+    - last_name."""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new User"""
+
+        super().__init__(*args, **kwargs)
