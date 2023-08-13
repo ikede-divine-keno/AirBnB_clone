a64ac3b8d06ee62042a90676a4c681a7ac24056b
ikede-divine-keno <ikededivine@gmail.com>
2023-08-13 12:10:21 -0700

    models/amenity.py added

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+    Defines a class Amenity.
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Represent an Amenity class."""
+    name = ""
