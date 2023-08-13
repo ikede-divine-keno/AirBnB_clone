c2b900f97a562f3578e4555f1b3353d8f63125e7
ikede-divine-keno <ikededivine@gmail.com>
2023-08-13 12:07:38 -0700

    models/city.py added

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+    Defines a class City.
+"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Represent a City class."""
+    state_id = ""
+    name = ""
